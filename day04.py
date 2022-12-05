5d5dc4f3cf8387ecd9e15c2dd428833d63bacb02
Sterre <xsterrestorm@outlook.com>
2022-12-05 02:28:03 +0100

    Day 4 pt 2

--- day04.py
+++ day04.py
@@ -1,5 +1,4 @@
-import numpy as np
-def overlap_area(filename):
+def full_overlap(filename):
     with open(filename) as inp:
         count = 0
         for line in inp:
@@ -12,4 +11,19 @@ def overlap_area(filename):
         print(count)
 
 
-overlap_area("input/day4full.txt")
+full_overlap("input/day4full.txt")
+
+def partial_overlap(filename):
+    with open(filename) as inp:
+        count = 0
+        for line in inp:
+            line.strip()
+            a, b = line.split(",")
+            a1, a2 = a.split("-")
+            b1, b2 = b.split("-")
+            if int(a1) <= int(b2) and int(a2) >= int(b1):
+                count += 1
+        print(count)
+
+
+partial_overlap("input/day4full.txt")
