93f6b0a11da715195289dea8a07a323521ede48d
Sterre <xsterrestorm@outlook.com>
2022-12-05 22:09:00 +0100

    Day 5 pt 1

---
+++ day05.py
@@ -0,0 +1,60 @@
+import re
+import pandas as pd
+import numpy as np
+
+
+def create_matrices(filename):
+    sequencematrix = []
+    stacks = []
+    M1 = []
+    with open(filename) as inp:
+        crates, directions = inp.read().split("\n\n")
+
+    # het nice maken
+    crates = crates.split('\n')
+    for elem in crates:
+        tempmatrix = []
+        for i in range(len(elem)):
+            if i % 4 == 1:
+                tempmatrix.append(elem[i])
+        M1.append(tempmatrix)
+    # stonx
+    for i in range(int(M1[-1][-1])):
+        temparray = []
+        for j in range(len(M1) - 1):
+            try:
+                tempvar = M1[j][i]
+                if tempvar != " ":
+                    temparray.append(tempvar)
+            except IndexError:
+                pass
+        temparray.reverse()
+        stacks.append(temparray)
+
+    # find directions matrix
+    directions = directions.split('\n')
+    for line in directions:
+        reg = re.findall(r"\d+", line)
+        sequence = [int(x) for x in reg]
+        sequencematrix.append(sequence)
+    return stacks, sequencematrix
+
+
+def doe_dan(filename):
+    stacks, directions = create_matrices(filename)
+
+    # big moving things
+    for elem in directions:
+        for i in range(0, elem[0]):
+            stacks[elem[2] - 1].append(stacks[elem[1] - 1][-1])
+            stacks[elem[1] - 1].pop()
+    print(stacks)
+    answer = []
+
+    # antwoordje zoeken denk
+    for elem in stacks:
+        answer.append(elem[-1])
+    print(answer)
+
+
+doe_dan("input/day5full.txt")
