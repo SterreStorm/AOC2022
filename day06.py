4abec0678c6934a28cf11ac5a4c8b1d8f74057e1
Sterre <xsterrestorm@outlook.com>
2022-12-06 18:58:58 +0100

    Day 6 pt 1 + 2

--- day06.py
+++ day06.py
@@ -3,12 +3,15 @@ def start_of_packet(filename):
     with open(filename) as f:
         # obtain string
         for line in f:
+            # split into proper substrings
             for i in range(13, len(line)):
                 count = 0
                 substring = line[i-13:i+1]
+                # check if no double characters
                 for j in range(len(substring)):
                     if substring.count(substring[j]) > 1:
                         count += 1
+                # print location
                 if count == 0:
                     print(i+1)
                     break
